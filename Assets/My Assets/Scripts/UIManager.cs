ed69a1ab406ab235c3037a12e16fdcbbe0809ae6
victorgordillo98 <79528597+victorgordillo98@users.noreply.github.com>
2022-09-15 12:34:27 -0500

    Implementación botones interacción usuario con piña guía
    Se implementan los botones que se van a utilizar en la interacción del usuario con la piña y se activan los las funciones de repetir la interacción o continuar a la siguiente interfaz, dependiendo de la elección del jugador.

--- Assets/My Assets/Scripts/UIManager.cs
+++ Assets/My Assets/Scripts/UIManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class UIManager : MonoBehaviour
+namespace FruitDiet
 {
-    // Start is called before the first frame update
-    void Start()
+    public class UIManager : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject currentInterface;
+        public void RepeatInteraction()
+        {
+            currentInterface.SetActive(true);
+        }
+        public void NextInteraction(GameObject nextInteraction)
+        {
+            currentInterface.SetActive(false);
+            currentInterface = nextInteraction;
+            currentInterface.SetActive(true);   
+        }
     }
 }
+
